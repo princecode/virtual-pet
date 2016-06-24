befa9fcaf60b6899831d0447ea514a45079bfb47
Prince <princemwilliams@gmail.com>
2016-06-24 17:05:03 -0400

    Started intro and main menu.

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -8,8 +8,34 @@ namespace VirtualPet
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+
+            // welcome
+            // instructions
+            Console.WriteLine("\n\nWAKANDA PANTHER TRAINER\n\nWould you like to train a baby panther totem?\n\nPlease enter \"1\" for \"Yes\" and \"2\" for \"No\"!\n");
+
+            int introSelect = Convert.ToInt32(Console.ReadLine());
+
+            if (introSelect == 1)
+            {
+                Menu();
+            }
+
+            Console.WriteLine("\nThat's unfortunate. Feel free to return at a later time!\n Press any key to exit.");
+            Console.ReadKey();
+
+            //private static void Menu()
+            //present possible options
+            //Create pet class
+        }
+        private static void Menu()
+        {
+
         }
     }
 }
+
+
